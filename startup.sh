de907641f3d14c2066e68cc4a1dfa11096d22089
Tim <ceoymw@gmail.com>
2019-06-11 09:22:23 +0200

    adds proper version of .xinitrc and startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 # Commands that you want to be run once connected can go here
+cd /home/pi/iqube-web
+/usr/bin/npm install
+/usr/bin/node /home/pi/iqube-web/index.js
+
+startx /home/pi/.xinitrc
